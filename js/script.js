40f098b7240d5f4e2ee0c1162a28ca51a637ecda
Mike Schairer <Mike.Schairer@gmail.com>
2026-08-18 21:49:57 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-AOS.init({
-  duration: 1200,
-})
-
 nav = document.getElementById("logo");
 
   var myScrollFunc = function () {
@@ -16,3 +12,7 @@ nav = document.getElementById("logo");
   window.addEventListener("scroll", myScrollFunc);
 
 // Change y>=200 value to affect the distance trigger for when the Nav bar is revealed. The lower the value, the earlier in the page it wil show. The script will also hide the nav when you scroll back up
+
+AOS.init({
+  duration: 1200,
+})
